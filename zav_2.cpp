f6922d5643d504410123e60826e2584f1c4085e2
Chichilivskiy <christinovka@mail.ru>
2016-06-04 18:22:56 +0300

    Add files via upload

---
+++ zav_2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+	int a,b,x;
+	printf("Vvedite a:");
+	scanf ("%d",&a);
+	printf ("Vvedite b:");
+	scanf ("%d", &b);
+	x=-(b/a);
+	printf ("%d",x);
+	return 0;
+}
